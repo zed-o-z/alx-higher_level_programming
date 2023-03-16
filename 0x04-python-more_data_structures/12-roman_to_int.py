b9d0fb772ff54b310c75513d169e3a91e0465f41
zerubabel <115575533+zed-o-z@users.noreply.github.com>
2023-03-16 14:43:56 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    prev_val = 0
-    for i in range(len(roman_string)):
-        for key, val in num.items():
-            if key == roman_string[i]:
-                total += val
-                if prev_val < val:
-                    total -= (prev_val * 2)
-                    prev_val = val
-    return total
+    res = 0
+    if type(roman_string) == str:
+        largest = 1
+        rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for s in roman_string[::-1]:
+            if s in rn:
+                if rn[s] >= largest:
+                    res = res + rn[s]
+                    largest = rn[s]
+                else:
+                    res -= rn[s]
+    return res
